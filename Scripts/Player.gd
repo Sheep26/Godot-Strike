d3f8e2658b520816da642bf7e569ac3f714fe222
Sheep26 <crabeykiller@gmail.com>
2024-01-14 17:30:46 +1300

    e

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,24 +1,22 @@
 extends CharacterBody3D
 
+@export var SPEED = 5.0
+@export var JUMP_VELOCITY = 4.5
+const MIN_LOOK_ANGLE: float = -75.0
+const MAX_LOOK_ANGLE: float = 75.0
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
