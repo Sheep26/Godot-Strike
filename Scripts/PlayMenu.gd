e7b4afcc9c21659f386b0cbc11e026b5feda1ae8
Sheep26 <crabeykiller@gmail.com>
2024-02-09 20:33:03 +1300

    e

--- Scripts/PlayMenu.gd
+++ Scripts/PlayMenu.gd
@@ -9,9 +9,6 @@ func _process(_delta):
 func _on_button_2_pressed():
 	Client._changeScene("res://Maps/Lobby.tscn")
 
-func _on_host_button_pressed():
-	pass
-
 func _on_join_button_pressed():
 	pass
 
